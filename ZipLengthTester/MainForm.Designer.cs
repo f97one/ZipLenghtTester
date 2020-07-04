9292902ad8e1394384247110d37f08f3be3b6932
HAJIME Fukuna <f97one@gmail.com>
2020-07-04 10:26:07 +0900

    Main Formの名称を変更。

--- ZipLengthTester/MainForm.Designer.cs
+++ ZipLengthTester/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ZipLengthTester
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Zip Length Tester";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
