37b6942ebfe95bc6d58830e3ae99343711626c22
HAJIME Fukuna <f97one@gmail.com>
2020-07-05 11:43:33 +0900

    Zipファイルかどうかを判断する処理を実装。

---
+++ ZipLengthTester/ZipFileOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipLengthTester
+{
+    class ZipFileOperation
+    {
+        private const int ZipLeadBytes = 0x04034b50;
+
+        public bool IsPkZipCompressedData(byte[] data)
+        {
+            Debug.Assert(data != null && data.Length > 4);
+            return (BitConverter.ToInt32(data, 0) == ZipLeadBytes);
+        }
+    }
+}
