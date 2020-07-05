7aec41ea5d19df5fa417313fa80ff1d274f49e91
HAJIME Fukuna <f97one@gmail.com>
2020-07-05 18:50:04 +0900

    exeに書き込むメタ情報を整備。

--- ZipLengthTester/Properties/AssemblyInfo.cs
+++ ZipLengthTester/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
 [assembly: AssemblyTitle("ZipLengthTester")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Count total length of each compressed file in zip-format")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Formula97.NET")]
 [assembly: AssemblyProduct("ZipLengthTester")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2020 HAJIME Fukuna")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
